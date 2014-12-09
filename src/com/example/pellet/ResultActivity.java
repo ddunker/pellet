eec9ffdc81c7dcc2cd0e9c8132d3465db1afbb89
ddunker <mbinkovskiy@gmail.com>
2014-12-09 23:33:04 +0200

    design changes

--- src/com/example/pellet/ResultActivity.java
+++ src/com/example/pellet/ResultActivity.java
@@ -11,14 +11,15 @@ import java.math.BigDecimal;
  * Created by binkovskiy on 05.12.14.
  */
 public class ResultActivity extends Activity {
-    private TextView productView;
-    private TextView distanceView;
-    private TextView woMarginView;
     private SeekBar marginChange;
     private TextView ttlMarginView;
     private TextView tmpMarginView;
     private TextView benefitView;
-    private TextView zpView;
+    private TextView zpValueView;
+    private TextView ttlSaleView;
+    private TextView tonaSaleView;
+    private TextView ttlMrgView;
+    private TextView tonaMrgView;
     float woMarg;
     float marg;
     float tonaMarg;
@@ -38,21 +39,29 @@ public class ResultActivity extends Activity {
                 getIntent().getFloatExtra("exp", 0);
 
 
-        productView = (TextView)findViewById(R.id.prodTextView);
-        distanceView = (TextView)findViewById(R.id.distanceTextView);
-        woMarginView = (TextView)findViewById(R.id.woMargeTextView);
+        TextView productView = (TextView) findViewById(R.id.prodTextView);
+        TextView distanceView = (TextView) findViewById(R.id.distanceTextView);
+        TextView woMarginView = (TextView) findViewById(R.id.woMarginTextView);
+        TextView woMarginValueView = (TextView) findViewById(R.id.woMarginValueTextView);
         marginChange = (SeekBar)findViewById(R.id.seekBar);
         ttlMarginView = (TextView)findViewById(R.id.wMarginTextView);
         tmpMarginView = (TextView)findViewById(R.id.tmpMarginTextView);
         benefitView = (TextView)findViewById(R.id.benefitTextView);
-        zpView = (TextView)findViewById(R.id.zpTextView);
+        TextView zpView = (TextView) findViewById(R.id.zpTextView);
+        zpValueView = (TextView)findViewById(R.id.zpValueTextView);
+        ttlSaleView = (TextView) findViewById(R.id.ttlSaleTextView);
+        tonaSaleView = (TextView) findViewById(R.id.tonaSaleTextView);
+        ttlMrgView = (TextView) findViewById(R.id.ttlMrgTextView);
+        tonaMrgView = (TextView) findViewById(R.id.tonaMrgTextView);
 
         productView.setText(getIntent().getStringExtra("product") + " (" + getIntent().getStringExtra("wrapping") + ")");
-
         distanceView.setText(getIntent().getStringExtra("from") + " - " + getIntent().getStringExtra("to") + " (" +
                 getIntent().getFloatExtra("distance", 0) + "km) - доставка: " + delivery);
-
-        woMarginView.setText("ИТОГО, без наценки: " + woMarg);
+        woMarginView.setText("ИТОГО, без наценки: ");
+        woMarginValueView.setText(String.valueOf(woMarg));
+        ttlMarginView.setText("Продажная цена, общая / 1т: ");
+        benefitView.setText("Прибыль, общая / 1т: ");
+        zpView.setText("ЗП от прибыли: ");
 
         marginChange.setProgress((getIntent().getIntExtra("margin", 0)));
         count();
@@ -74,15 +83,12 @@ public class ResultActivity extends Activity {
         ttlBenefit = marg - woMarg;
         tonaBenefit = ttlBenefit / getIntent().getFloatExtra("weight", 0);
         zp = (float) (ttlBenefit * 0.3);
-        ttlMarginView.setText("Продажная цена, общая / 1т: " +
-                String.valueOf(BigDecimal.valueOf(marg).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()) +
-                " / " + String.valueOf(BigDecimal.valueOf(tonaMarg).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
-        tmpMarginView.setText("Наценка: " + String.valueOf(marginChange.getProgress()));
-        benefitView.setText("Прибыль, общая / 1т: " +
-                String.valueOf(BigDecimal.valueOf(ttlBenefit).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()) +
-                " / " + String.valueOf(BigDecimal.valueOf(tonaBenefit).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
-        zpView.setText("ЗП от прибыли: " +
-                String.valueOf(BigDecimal.valueOf(zp).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
+        ttlSaleView.setText(String.valueOf(BigDecimal.valueOf(marg).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
+        tonaSaleView.setText(String.valueOf(BigDecimal.valueOf(tonaMarg).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
+        tmpMarginView.setText("Наценка: " + String.valueOf(marginChange.getProgress()) + "%");
+        ttlMrgView.setText(String.valueOf(BigDecimal.valueOf(ttlBenefit).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
+        tonaMrgView.setText(String.valueOf(BigDecimal.valueOf(tonaBenefit).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
+        zpValueView.setText(String.valueOf(BigDecimal.valueOf(zp).setScale(2, BigDecimal.ROUND_HALF_DOWN).floatValue()));
     }
 }
 
